f81c8b09638805499bae142ec8fdc2db97c794ad
Jose David Alcaraz <jose.david.alcaraz7@gmail.com>
2026-04-16 16:28:03 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      // include: [/linked-dep/, /node_modules/],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
